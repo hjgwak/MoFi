8d28f8082d3870bf827c0f007ce63852ace1ba70
hjgwak <khj2830@gmail.com>
2017-10-17 20:12:57 +0900

    GibssSampling & Entropy
    edit GibssSampling
    add Entropy

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include "Sequence.h"
 #include "SequenceReader.h"
 
@@ -17,9 +18,11 @@ typedef map<string, string> Motifs;
 void printUSAGE();
 void printMotifs(const Motifs& motifs);
 void printProfile(const Profile& profile);
+void printState(const Motifs& motifs, const Profile& profile, const double entropy, const string& header);
 Profile makeProfile(const Motifs& motifs, const int k);
 Profile initiate(const Seqs& seqs, const int k, Motifs& motifs);
 void expect(const Seqs& seqs, const int n, const int k, Motifs& motifs, const Profile& profile);
+double Entropy(const Profile& profile);
 
 int main(int argc, char* argv[]) {
     if (argc < 5 || strcmp(argv[1], "-h") == 0) printUSAGE();
@@ -33,36 +36,63 @@ int main(int argc, char* argv[]) {
         if (strcmp(argv[i], "-k") == 0)      k = atoi(argv[i + 1]);
         else if (strcmp(argv[i], "-i") == 0) reader.open(argv[i + 1]);
     }
+	
+	if (!reader.is_open()) {
+		cerr << "Input file was not opened." << endl;
+		system("pause");
+		return -1;
+	}
 
     auto seqs = reader.getAll();
 
     cout << "k:         " << k << endl;
     cout << "# of seqs: " << seqs.size() << endl;
 
-    map<string, string> motifs;
-    auto   profile  = initiate(seqs, k, motifs);
+    Motifs  motifs, best_motifs;
+    Profile best_profile;
+    auto    profile      = initiate(seqs, k, motifs);
+    double  best_entropy = Entropy(profile);
 
-    cout << "[Init]" << endl;
-    printMotifs(motifs);
-    printProfile(profile);
+    printState(motifs, profile, best_entropy, "[Init]");
 
     int step = 0;
-    while (step < 100) {
+    bool run = true;
+    while (run) {
+        double  local_best = 3.0 * k;
+        Motifs  local_best_motifs;
+        Profile local_best_profile;
         for (int i = 0; i < seqs.size(); ++i) {
-            expect(seqs, i, k, motifs, profile);
+            motifs.erase((seqs.begin() + i)->getHeader());
             profile = makeProfile(motifs, k);
+            expect(seqs, i, k, motifs, profile);
 
+            double entropy = Entropy(profile);
+            if (entropy < local_best) {
+                local_best = entropy;
+                local_best_motifs = motifs;
+                local_best_profile = profile;
+            }
             /*
             cout << "[Step" << i << "]" << endl;
             printMotifs(motifs);
             printProfile(profile);
              */
         }
-        cout << "[Step" << step << "]" << endl;
-        printMotifs(motifs);
-        printProfile(profile);
-        step += 1;
+        printState(local_best_motifs, local_best_profile, local_best, "[Step" + to_string(step) + "]");
+        cout << "Global Best: " << best_entropy << endl;
+        step++;
+
+        if (local_best < best_entropy) {
+            best_entropy = local_best;
+            best_motifs  = local_best_motifs;
+            best_profile = local_best_profile;
+        }
+        else {
+            run          = false;
+        }
     }
+
+    printState(best_motifs, best_profile, best_entropy, "[BEST]");
 
     return 0;
 }
@@ -97,6 +127,13 @@ void printProfile(const Profile& profile) {
         cout << endl;
     }
     cout << endl;
+}
+
+void printState(const Motifs& motifs, const Profile& profile, const double entropy, const string& header) {
+    cout << header << endl;
+    printMotifs(motifs);
+    printProfile(profile);
+    cout << "Entropy: " << entropy << endl;
 }
 
 Profile makeProfile(const Motifs& motifs, const int k) {
@@ -148,4 +185,15 @@ void expect(const Seqs& seqs, const int n, const int k, Motifs& motifs, const Pr
     }
 
     motifs[target->getHeader()] = max_kmer;
+}
+
+double Entropy(const Profile& profile) {
+    double entropy = 0.0;
+    for (auto& pos : profile) {
+        for (auto& prob : pos) {
+            entropy -= prob.second * log2(prob.second);
+        }
+    }
+
+    return entropy;
 }
